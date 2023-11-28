2313093943ec7ceec2608ebd8ba488e61ce2dfdb
Jon Stapleton <stapleton.jon.michael@gmail.com>
2023-11-28 14:03:36 -0500

    added lessons index area on browse page

--- src/routes/library/browse/+page.svelte
+++ src/routes/library/browse/+page.svelte
@@ -2,6 +2,7 @@
     import {onMount} from 'svelte'
     import GroupCard from '$lib/components/GroupCard.svelte';
     import Projects from '$lib/components/Projects.svelte';
+    import { srcToUrl } from '$lib/utils/pathUtils';
     export let data;
     let lessonGroups:any = []
 
@@ -24,4 +25,33 @@
     <div class='section'>
         <Projects list={true} elems={lessonGroups} />
     </div>
+    <div class='section'>
+        <h1>CodeVA's Curriculum Lesson Index</h1>
+        <p class='block'>Here, you'll find a table with every single lesson plan and lesson sequence in CodeVA's library.</p>
+        <table class='table'>
+            <thead>
+                <tr class='headings'>
+                    <td>Title</td>
+                    <td>Grade(s)</td>
+                    <td>Subject(s)</td>
+                    <td>Type(s)</td>
+                </tr>
+            </thead>
+            <tbody>
+            {#each data.lessons as lesson}
+            <tr>
+                <td><a href={srcToUrl(lesson.pathData.path)}>{lesson.title}</a></td>
+                <td>{#each lesson.grades as grade}{grade}{/each}</td>
+                <td>{#each lesson.subjects as subj, i}{subj}{#if lesson.subjects.length > 1 && i < lesson.subjects.length-1}, {/if}{/each}</td>
+                <td>{#each lesson.types as type}{type}{/each}</td>
+            </tr>
+            {/each}
+            </tbody>
+        </table>
+    </div>
 </div>
+<style>
+    tbody > tr:hover {
+        background-color: whitesmoke;
+    }
+</style>
