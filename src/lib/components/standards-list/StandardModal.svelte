34af5acec04b307b48b3702ef367aae2e8e38ef1
Jon Stapleton <stapleton.jon.michael@gmail.com>
2023-11-28 13:30:12 -0500

    fixed standards display, pretty scuffed but good for now

--- src/lib/components/standards-list/StandardModal.svelte
+++ src/lib/components/standards-list/StandardModal.svelte
@@ -3,16 +3,31 @@
     import {faClose} from '@fortawesome/free-solid-svg-icons'
     export let active = false
     export let standards:any
+    export let id:string
     import { onMount } from 'svelte';
     import SingleStandardModalBody from './SingleStandardModalBody.svelte';
     import TableRow from './TableRow.svelte';
 
     export function deactivate() {
-        active = false
+        if(back) {
+            active = true
+            el = null
+            back = false
+        } else {
+            active = false
+        }
     }
     export function activate() {
         active = true
     }
+
+    function select(event) {
+        el = SingleStandardModalBody
+        selected = event.detail.standard
+        back = true
+    }
+    let selected:Standard
+    let back:boolean = false
 
     // TODO: implement
     // If there's only one standard, it gets pushed to el
@@ -22,6 +37,7 @@
     // Clicking "back" in the SingleStandardModal modifies el to null
     let el
     onMount(() => {
+        selected = standards[0]
         if(standards.length == 1) {
             el = SingleStandardModalBody
         }
@@ -31,43 +47,13 @@
 <div class="modal {active? 'is-active' : ''}">
     <div on:click={deactivate} class="modal-background"></div>
     <div class="modal-card">
-        <!-- <section class='modal-card-body'> -->
-            {#if el}
-                <svelte:component this={el} on:close={deactivate} standard={standards[0]} />
-                <!-- <SingleStandardModalBody standard={standards[0]} /> -->
-            {:else}
-                {#each standards as standard}
-                <TableRow standard={standard} />
-                {/each}
-            {/if}
-        <!-- </section> -->
-        <!-- <header class='modal-card-head'>
-            <h1 class='modal-card-title'>{standard.title}
-                <span class='ml-5 tag is-dark'>Algorithms & Programming</span>
-                <span class='tag is-dark'>Kindergarten</span>
-            </h1>
-            <button on:click={deactivate} class="button closer" aria-label="close"><Fa size='1.25x' icon={faClose} /></button>
-        </header>
-        <section class='modal-card-body'>
-            <p>{standard.text}</p>
-            {#if standard.subs.length > 0}
-            <ol type='a'>
-                {#each standard.subs as sub}
-                <li>{sub}</li>
-                {/each}
-            </ol>
-            {/if}
-        </section>
+        {#if el}
+            <SingleStandardModalBody back={back} on:close={deactivate} standard={selected} />
+        {:else}
+            <TableRow on:select={select} on:close={deactivate} id={id} standards={standards} />            
+        {/if}
         <footer class='modal-card-foot'>
             <slot name="footer" />
-        </footer> -->
-        
-        <!-- <footer class="modal-card-foot">
-            <button on:click={handler} class="button {standardSelected? 'is-dark':'is-success'} is-hovered">
-                <Fa class='mr-3' icon={icon} />
-                {standardSelected? "Remove From":"Add To"} Search Filter
-            </button>
-            <button on:click={()=>active=false} class="button">Close</button>
-        </footer> -->
+        </footer>
     </div>
 </div>
--- src/lib/components/standards-list/TableRow.svelte
+++ src/lib/components/standards-list/TableRow.svelte
@@ -1,9 +1,67 @@
 <script lang='ts'>
     import type { Standard } from "$lib/utils/metaUtils";
+    import Fa from 'svelte-fa'
+    import {faClose} from '@fortawesome/free-solid-svg-icons'
 
-    export let standard:Standard
+    export let standards:Standard[] = []
+    export let id:string
+
+    import { createEventDispatcher, onMount } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+    function sendClose() {
+		dispatch('close');
+	}
+    function selectStandard(standard:Standard) {
+        dispatch('select', {standard: standard})
+    }
+
+    onMount(() => {
+        // TODO: Get info from ID for header
+
+    })
 </script>
 
-<div class='standard-table-row'>
-    <p>{standard.id}</p>
+<div class='modal-card-head'>
+    <h1 class='modal-card-title'>{id}
+        <!-- <span class='ml-5 tag is-dark'>{standard.strand}</span> -->
+        <!-- <span class='tag is-dark'>{standard.grade}</span> -->
+    </h1>
+    <button on:click={sendClose} class="button closer" aria-label="close"><Fa size='1.25x' icon={faClose} /></button>
 </div>
+<div class='modal-card-body'>
+    <table class='table'>
+        <tr class='headings'>
+            <th style='width: 3rem;'>Title</th>
+            <th style='width: 4rem;'>ID</th>
+            <th>Text</th>
+        </tr>
+        {#each standards as standard, i}
+        <tr on:click={() => selectStandard(standard)} class='stds'>
+            <td>{standard.title}</td>
+            <td><span class='tag is-dark is-small'>{standard.id}</span></td>
+            <td class='text'>
+                {standard.text}
+                {#each standard.subs as sub}
+                    {sub}
+                {/each}
+            </td>
+        </tr>
+        {/each}
+    </table>
+</div>
+
+<style>
+    .text {
+        max-width: 8rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        padding-right: 0;
+        margin-right:0;
+    }
+    .stds:hover {
+        background-color: whitesmoke;
+        cursor: pointer;
+    }
+</style>
