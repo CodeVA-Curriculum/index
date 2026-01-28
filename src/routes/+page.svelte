630da7a9d4955840ac6ddd91a31274e2c684bffc
jonstapleton <kat@octojon.dev>
2026-01-28 13:07:41 -0500

    got section wrapping to work responsively

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,22 +4,22 @@
   import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 </script>
 <div class='container main'>
-  <section>
-    <h1>Welcome to the CodeVA Curriculum Hub!</h1>
-    <p>This site contains resources, tools, lessons, and other high-quality, thoughtful materials for people learning computing and the people who teach them.</p>
-    <a href="/learn" role="button">Explore Coding Resources <span><Fa icon={faArrowRight} /></span></a>
-    <a href="/teach/library" role="button">Browse the Lesson Library <span><Fa icon={faArrowRight} /></span></a>
+  <section class='video-section'>
+    <div>
+      <h1>Welcome to the CodeVA Curriculum Hub!</h1>
+      <p>This site contains resources, tools, lessons, and other high-quality, thoughtful materials for people learning computing and the people who teach them.</p>
+      <a href="/learn" role="button">Explore Coding Resources <span><Fa icon={faArrowRight} /></span></a>
+      <a href="/teach/library" role="button">Browse the Lesson Library <span><Fa icon={faArrowRight} /></span></a>
+    </div>
+    <div>
+      <Video id='14I8C6FKINQ' />
+    </div>
   </section>
-  <section>
-    <Video id='14I8C6FKINQ' />
+  <section class='is-half video'>
   </section>
 </div>
 
 <style lang='scss'>
-  .main {
-    display: flex;
-    section { flex: 1; }
-  }  
   a {
     width: 100%;
     margin: 12px 0;
