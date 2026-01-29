aacd7b053df2835783b544dca83f1cb6f4e0893b
octojon <mail@octojon.dev>
2026-01-29 14:59:12 -0500

    finished search page

---
+++ src/lib/components/Help.svelte
@@ -0,0 +1,31 @@
+<script lang='ts'>
+  import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons'
+  import Fa from 'svelte-fa'
+</script>
+
+<span><Fa size=.9x icon={faCircleQuestion} />
+<div class='tooltip'>
+  <span>A Lesson Plan is one continuous learning experience, usually about 1 hour of instruction. Some are longer, spreading instruction out over several hours or class periods. Check the lesson details for an estimate of lesson duration.</span>
+</div>
+</span>
+
+<style lang='scss'>
+  .tooltip {
+    visibility: hidden;
+    position: absolute;
+    bottom: 36px;
+    left: calc(-100px + 0.25rem); 
+    line-height: 120%;
+    width: 200px;
+    padding: 8px;
+    font-size: medium;
+    border-radius: 8px;
+    background-color: gray;
+  }
+  span {
+    position: relative;
+    &:hover {
+      .tooltip { visibility: visible; }
+    }
+  }
+</style>
--- src/routes/teach/library/+page.svelte
+++ src/routes/teach/library/+page.svelte
@@ -1,18 +1,20 @@
 <script lang='ts'>
   import SearchBar from './components/SearchBar.svelte'
 </script>
-<section>
-  <SearchBar />
-</section>
-<section class='video-section'>
-  <div>
-    <h1>The CodeVA Curriculum Library</h1>
-    <p>Lorem ipsum dolor sit amet.</p>
-  </div>
-  <div>
-    <p>Carousel</p>
-  </div>
-</section>
-<section>
-  <h2>Browse Collections</h2>
-</section>
+<div class='container'>
+  <section>
+    <SearchBar />
+  </section>
+  <section class='video-section'>
+    <div>
+      <h1>CodeVA Curriculum Library</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+    <div>
+      <p>Carousel</p>
+    </div>
+  </section>
+  <section>
+    <h2>Browse Collections</h2>
+  </section>
+</div>
