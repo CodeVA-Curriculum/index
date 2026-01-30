433984b3987a8bc1080d0b2577d4fd5147f36e19
jonstapleton <kat@octojon.dev>
2026-01-30 15:00:39 -0500

    subject and course seed script working

--- src/lib/server/db/seed.ts
+++ src/lib/server/db/seed.ts
@@ -1,12 +1,15 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 import * as schema from './schema'
+import { read } from 'to-vfile'
+import YAML from 'yaml'
+import { seedSubjects } from './seedSubjects'
 
 async function main() {
   const client = new Database(process.env.DATABASE_URL);
 
   const db = drizzle(client, { schema: { ...schema } });
   const users = await db.select().from(schema.user)
-  console.log(users)
+  const { subjects, courses } = await seedSubjects(db, schema)
 }
 main()
