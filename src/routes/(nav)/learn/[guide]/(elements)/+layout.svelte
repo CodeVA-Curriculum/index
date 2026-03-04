aee984be3d4eb56ba450647510ac3a60a10d035f
octojon <mail@octojon.dev>
2026-03-04 12:19:26 -0500

    added [guide]/projects

---
+++ src/routes/(nav)/learn/[guide]/(elements)/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang='ts'>
+  import GuideNav from '$lib/components/guide/GuideNav.svelte'
+  let { data, children } = $props()
+
+  import {onMount} from 'svelte'
+  onMount(() => {
+    console.log(data)
+  })
+</script>
+<GuideNav guide={data.guide} />
+<div class='container'>
+  {@render children()}
+</div>
