babf676231fc90459704b0a3b33e3ec5ac285285
octojon <mail@octojon.dev>
2026-03-04 12:19:26 -0500

    added [guide]/projects

---
+++ src/routes/(nav)/learn/[guide]/(elements)/projects/+page.server.ts
@@ -0,0 +1,14 @@
+import type {PageServerLoad} from './$types'
+import { getGuideFromParam, getProjectsFromGuide } from '$lib/server/db/utils'
+import { db } from '$lib/server/db/index'
+import type { Guide } from '$lib/server/db/schema'
+import { project } from '$lib/server/db/schema'
+
+export const load:PageServerLoad = async ({ parent}) => {
+  // Pull projects from db
+  let { guide } = await parent()
+  const projects = await getProjectsFromGuide(guide.id)
+  return {
+    projects: projects
+  }
+}
