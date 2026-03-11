7ca28fbb9716f202890c944e018b132df890fb22
octojon <mail@octojon.dev>
2026-03-11 11:05:00 -0400

    added hover

--- src/lib/components/guide/Node.svelte.ts
+++ src/lib/components/guide/Node.svelte.ts
@@ -1,23 +1,33 @@
 import type { Node as DbNode } from '$lib/server/db/schema'
+import { Lerp } from '$lib/utils'
 
 export class Node {
   db:DbNode
-  complete:boolean = false
-  x = 0; y = 0; radius = 50;
-  hover = $state(false)
+  complete:boolean = $state(false)
+  x = 0; y = 0; radius = 50
+  radiusLerp
+  hover = false
   scale = 1
   constructor(obj:DbNode) {
     this.db = obj
     this.x = obj.x
     this.y = obj.y
+    this.radiusLerp = new Lerp(50, 40)
   }
   draw(p5:any) {
+    this.radius = this.hover ? 100 : 50;
+    if(this.hover) {
+      this.radiusLerp.setTarget(100)
+    } else {
+      this.radiusLerp.setTarget(50)
+    }
+    this.radiusLerp.update(p5)
     if(this.x && this.y) {
-      p5.circle(this.x*this.scale, this.y*this.scale, this.hover ? this.radius * 1.5 : this.radius)
+      p5.circle(this.x*this.scale, this.y*this.scale, this.radiusLerp.get())
       p5.text(this.db.title, this.x*this.scale, this.y*this.scale)
       // p5.text(this.hover, this.x*this.scale, this.y*this.scale+10)
-      p5.text(this.x+", "+this.y, this.x*this.scale, this.y*this.scale+10)
-      p5.text(this.hover, this.x*this.scale, this.y*this.scale+20)
+      p5.text(this.radiusLerp.get(), this.x*this.scale, this.y*this.scale+10)
+      p5.text(this.x + ", " + this.y, this.x*this.scale, this.y*this.scale+20)
     } else {
       console.warn("Tried to render "+this.db.path+" to map, but node is not present in map.canvas!")
     }
