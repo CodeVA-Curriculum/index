85abed497f9193a58bbf3ef37af9743a44929d9c
octojon <mail@octojon.dev>
2026-03-26 16:59:04 -0400

    added support for project path vis, need to add optional nodes next

--- src/lib/components/guide/Project.svelte.ts
+++ src/lib/components/guide/Project.svelte.ts
@@ -1,7 +1,7 @@
 import type { Project as DbProject } from '$lib/server/db/schema'
 import type { Map } from "./Map.svelte"
 import type { Node } from './Node.svelte'
-import type { Edge } from './Edge.svelte'
+import { Edge } from './Edge.svelte'
 import { NodeInGroup } from './Node.svelte'
 
 interface Input {
@@ -31,13 +31,13 @@ export class Project {
     // build node groups and edges
     for(const g of i.nodeGroups) {
       let group = new Group(g, i.pivot, elementsByPath)
-      group.addEdges(allEdges, this.db, elementsByPath)
+      group.addEdges(allEdges, i.pivot, elementsByPath)
+      console.log(group.edges)
       this.nodeGroups.push(group)
     }
     
   }
   highlight() {
-    console.log(this.nodeGroups[0])
     this.highlighted = !this.highlighted
     for(const g of this.nodeGroups) {
       g.highlight()
@@ -45,9 +45,15 @@ export class Project {
   }
   toggleComplete() { this.complete = !this.complete }
   markComplete() { this.complete = true }
+  setup(p5) {
+    for(const g of this.nodeGroups) {
+      g.setup(p5)
+    }
+  }
   draw(p5){
-
-    // this is for debugging purposes only
+    for(const g of this.nodeGroups) {
+      g.draw(p5)
+    }
   }
 }
 
@@ -56,24 +62,43 @@ class Group {
   nodes:Node[] = []
   nodeMask:boolean = []
   edges:Edge[] = []
-  edgeMask:boolean[] = []
   constructor(db:any, order:object, elementsByPath:any) {
     this.title = db.alias
     order.sort((a,b) => a.index - b.index)
     for(const c of order) {
       const node = (db.nodes.filter((o) => o.id == c.nodeId))[0]
       this.nodes.push(elementsByPath[node.path])
+      this.nodeMask.push(order.optional)
     }
   }
-  addEdges(edges:Edge[], parent:Project, elementsByPath:any) {
+  setup(p5) {
+    for(const e of this.edges) {
+      e.setup(p5)
+    }
+    console.log("Group has edges", this.edges.length)
+  }
+  draw(p5) {
+    for(const e of this.edges) {
+      e.projectDraw(p5)
+    }
+  }
+  addEdges(edges:Edge[], order:object, elementsByPath:any) {
     // TODO: Find the edge between each consecutive node in `this.nodes` and add it to `this.edges`
     let ids:number[] = []
     for(const node of this.nodes) {
       ids.push(node.db.id)
     }
-    const e = edges.filter((o) => ids.includes(o.to.db.id) && ids.includes(o.from.db.id) && Math.abs(ids.indexOf(o.from.db.id) - ids.indexOf(o.to.db.id)) == 1)
-    console.log(`Found ${e.length} edges for group ${this.title} in project ${parent.path}`)
-    this.edges = e
+    const e = edges.filter((o) => ids.includes(o.toNode.id) && ids.includes(o.fromNode.id) && Math.abs(ids.indexOf(o.fromNode.id) - ids.indexOf(o.toNode.id)) == 1)
+    // console.log(`Found ${e.length} edges for group ${this.title} in project ${parent.path}`)
+    for(const dbEdge of e) {
+      const o = new Edge(dbEdge, elementsByPath)
+
+      // const node0optional = this.nodeMask[this.nodes.findIndex((n) => n.db.id == o.to.db.id)]
+      // const node1optional = this.nodeMask[this.nodes.findIndex((n) => n.db.id == o.from.db.id)]
+      o.optional = true
+      this.edges.push(o)
+    }
+    // console.log("Edgemask success", this.edgeMask.length == this.edges.length)
   }
   highlight() {
     for(const n of this.nodes) {
