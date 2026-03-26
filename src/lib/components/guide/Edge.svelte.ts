cbf2e467d927caca775852431f02a39057b6d45d
octojon <mail@octojon.dev>
2026-03-26 16:59:04 -0400

    added support for project path vis, need to add optional nodes next

--- src/lib/components/guide/Edge.svelte.ts
+++ src/lib/components/guide/Edge.svelte.ts
@@ -6,8 +6,10 @@ interface ControlPoint {
 }
 export class Edge {
   db:DbEdge;
-  shape:any = $state();
-  highlight:any = $state();
+  optional:boolean = false
+  optionEdge:any 
+  shape:any
+  highlight:any
   from:Node;
   to:Node
   x = 0; y = 0;
@@ -43,18 +45,22 @@ export class Edge {
 
     this.highlight = p5.createGraphics(w,h)
     this.highlight.fill('rgba(0, 0, 0, 0)')
-    // if(this.optional) {
-    //   this.highlight.stroke(this.highlight.color(0, 255, 0))
-    // } else {
-      this.highlight.stroke(this.highlight.color(255, 0, 0))
-    // }
+    this.highlight.stroke(255, 0, 0)
     this.highlight.strokeWeight(12)
     this.highlight.bezier(p0.x-this.x, p0.y-this.y ,p1.x-this.x, p1.y-this.y ,p2.x-this.x, p2.y-this.y, p3.x-this.x, p3.y-this.y)
 
+    this.optionEdge = p5.createGraphics(w,h)
+    this.optionEdge.fill('rgba(0, 0, 0, 0)')
+    this.optionEdge.stroke(255, 0, 0)
+    this.optionEdge.strokeWeight(12)
+    this.optionEdge.bezier(p0.x-this.x, p0.y-this.y ,p1.x-this.x, p1.y-this.y ,p2.x-this.x, p2.y-this.y, p3.x-this.x, p3.y-this.y)
   }
   draw(p5) {
     p5.image(this.highlighted?  this.highlight : this.shape, this.x, this.y)
     // p5.image(this.highlight, this.x, this.y)
+  }
+  projectDraw(p5) {
+    if(this.highlighted) { p5.image(this.optional ? this.optionEdge : this.highlight, this.x, this.y)}
   }
 }
 
