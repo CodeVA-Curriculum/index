954eb3430d96799db2dd286b829c3dbb515aec20
jonstapleton <kat@octojon.dev>
2026-03-31 17:03:26 -0400

    added notloggedin page

---
+++ src/routes/(nav)/learn/[guide]/(elements)/account/+page.svelte
@@ -0,0 +1,31 @@
+<script lang='ts'>
+</script>
+
+<div class='account container narrow'>
+  <section>
+    <h1>Track Your Learning</h1>
+    <p>Log in with a teacher or student account to save projects, create reports, and track your progress! If you are a teacher interested in learning more about using trail guides in your classroom, <a href="https://codevirginia.org/">check out our professional learning!</a></p>
+    <div class='buttons'>
+      <a disabled role="button">Log In as a Teacher</a>
+      <a disabled role="button">Log In as a Student</a>
+    </div>
+    <blockquote>
+      When users are logged in as students, this site does not track their data and does not make any attempt to associate their data with their individual identity. If you have questions about our privacy practices, please contant <a mailto="jonstapleton@codevirginia.org">jonstapleton@codevirginia.org</a>.
+    </blockquote>
+  </section>
+</div>
+<style lang='scss'>
+  .narrow {
+    max-width: 796px;
+  }
+  .buttons {
+    display: flex;
+    gap: 1rem;
+    & > * {
+      flex: 1 1;
+    }
+  }
+  blockquote {
+    font-size: 0.75rem;
+  }
+</style>
