330ef4e8680a9eaffb90b6d57f4a0cfd62c083ba
jonstapleton <kat@octojon.dev>
2026-06-16 15:14:06 -0400

    added pacing guide instructions

--- src/routes/(nav)/teach/tools/pacing-guide-creator/+page.svelte
+++ src/routes/(nav)/teach/tools/pacing-guide-creator/+page.svelte
@@ -9,7 +9,12 @@
   <section>
     <Dismiss>
       <h1>Pacing Guide Creator</h1>
-      <p>Lorem ipsum</p>
+      <p>This tool is designed to help you find opportunities to <strong>integrate computer science</strong> into your curriculum! Follow the instructions below:</p>
+        <ol>
+          <li>Write a description of a unit in the first row (this can be a single lesson, a series of lessons, a whole semester, whatever)</li>
+          <li>Add related standards by clicking "Add". For example, if your unit covers oral language skills in 1st grade, you'd pick the relevant standard from the "Add" menu (1.EL.OL.1, in this case).</li>
+          <li>Click "Load Resources" to populate a list of CS activity examples that integrate computing standards with the other standards you selected!</li>
+        <p>Use the activity examples in your unit, or reference them for examples and inspiration for your own CS-integrated lessons!</p>
     </Dismiss>
   </section>
   <section>
