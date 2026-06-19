47c88e676b0c0668faebe4786accab020c8f3fdb
octojon <apollo@octojon.dev>
2026-06-19 15:59:18 -0400

    prep for v1 deployment

--- src/routes/(nav)/teach/library/browse/[...path]/pdf/+server.ts
+++ src/routes/(nav)/teach/library/browse/[...path]/pdf/+server.ts
@@ -9,11 +9,6 @@ import path from 'path';
 
 export const GET: RequestHandler = async ( { request, locals } ) => {
   // TODO: check permissions on locals.user
-  let id = el.links.drive.substring(el.links.drive.indexOf('/d/') + 3)
-  id = id.substring(0, id.indexOf('/'))
-  if(id.length > 0) {
-      ids.push(id)
-  }
   const id = "16R6WSD0FJly_2HSAeIyMu7Oocb_A7n80UGUVLzwdCwo"
   const filePath = "./documents/" + id + ".pdf"
   console.log("Got call for PDF", id)
